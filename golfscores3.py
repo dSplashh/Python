e669ed7ba3309a2b00503de6dba9a54720ae703b
Darryl Alexander Fleurantin <71136614+dSplashh@users.noreply.github.com>
2022-07-26 21:26:21 -0400

    Update golfscores3.py

--- golfscores3.py
+++ golfscores3.py
@@ -3,34 +3,23 @@
 #CSC 110 Homework 3 Python lists ~ Part 3
 
 #list to store scores
-
 golfScores = []
 
 #list to store scores at rate
-
 rateScore = []
 
 #ask user for how many scores
-
 scoreNum = int(input("How many golf scores have you collected? "))
 
 #for loop to get scores
-
 for i in range(scoreNum):
-
     scores = int(input("Enter score: "))
-
     golfScores.append(scores)
-
 rate = int(input("Enter sample rate (k): "))
 
 #for loop to get scores at rate
-
 for j in range(len(golfScores)):
-
     if j % rate == 0:
-
-        rateScore.append(golfScores[j])
-        
+        rateScore.append(golfScores[j])        
 print("The sampled scores are:")        
 print(rateScore)
